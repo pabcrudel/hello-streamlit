6ccf3969881e886929129d52cd7c37a9d6f4bb5e
pablocru <pablocru@outlook.com>
2024-04-09 17:33:42 +0200

    feat: Display the same data frame as a literal, using the native function and with styles

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
+# Creates an `h1`
 st.title("Hello world")
+
+# Defines a table
+df = pd.DataFrame({
+  'City': ["Valencia", "Malaga"],
+  'Population': [791.413, 571.026]
+})
+
+# Renders content without using Streamlit's functions
+"""
+This paragraph and the following table, that are a literal and a variable
+respectively, are automatically printed without using `st.write()`.
+"""
+
+st.write("## Interactive Table")
+df
+st.write("You can accomplish that using `st.write()` too.")
+
+st.write("## Static Table")
+st.write("With the method `st.table(dl)` Streamlit generates a static table.")
+st.table(df)
+
+dataframe = pd.DataFrame(
+    np.random.randn(5, 8),
+    columns=('col %d' % i for i in range(8)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
