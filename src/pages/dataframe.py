fabcebbdba0bc541e22196fa3947a33c34f46619
pablocru <pablocru@outlook.com>
2024-05-05 18:22:49 +0200

    fix: Use "and" instead of "in" in each lambda validation

--- src/pages/dataframe.py
+++ src/pages/dataframe.py
@@ -50,15 +50,20 @@ def print_error_name():
     st.subheader("Add a error name if there is one")
 
     df = pd.DataFrame({
-        'event_name': ['Ev1', 'Ev2', 'Ev3', 'Ev4', None],
-        'validation_A': [True, False, False, None, None],
-        'validation_B': [False, True, False, None, None]
+        'event_name': ['Ev1', 'Ev2', 'Ev3', 'Ev4', 'Ev5', None],
+        'validation_A': [True, False, False, None, True, None],
+        'validation_B': [False, True, False, None, True, None],
+        'validation_C': [True, True, True, None, True, None]
     })
 
     df['error_name'] = df.apply(
         (
             lambda row: row['event_name']
-            if not (row['validation_A'] or row['validation_B'])
+            if not (
+                row['validation_A'] and
+                row['validation_B'] and
+                row['validation_C']
+            )
             else None
         ),
         axis=1
